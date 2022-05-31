21287cd3478391d813479f04dba8d0781f81a32d
Conrado Manclossi <conrado.macalossi@gmail.com>
2022-05-31 15:07:44 -0300

    Trips mod
    A struct for implement a vector of Trips, with POST, GET, UPDATE and DELETE methods.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,27 @@
 use chrono::{DateTime, Utc};
 
 mod modules;
-use modules::trip::Trip;
-use modules::category::Category;
+use modules::trip::Trips;
 use modules::record::Record;
+use modules::category::Category;
 
 fn main() {
     let now: DateTime<Utc> = Utc::now();
 
-    let mut trip_teste = Trip::add(1, now);
-    println!("{:?}", trip_teste);
+    let mut trips: Trips = Trips::new();
+    trips.post(1, now, None);
+    trips.post(2, now, None);
+    trips.post(3, now, None);
+    println!("{:?}", trips.get(2));
+    trips.put(2, None, Some(now));
+    println!("{:?}", trips.get(2));
+    trips.delete(2);
+    println!("{:?}", trips);
 
-    trip_teste.end_trip(now);
-    println!("{:?}", trip_teste);
 
     let category_teste: Category = Category::add(1, "Diesel");
     println!("{:?}", category_teste);
 
-    let record_teste: Record = Record::add(1, trip_teste.id, category_teste.id, "Diesel", now, 100, 1.0, 100.0);
+    let record_teste: Record = Record::add(1, 1, category_teste.id, "Diesel", now, 100, 1.0, 100.0);
     println!("{:?}", record_teste);
 }
--- backend/src/modules/trip.rs
+++ backend/src/modules/trip.rs
@@ -20,20 +20,67 @@ impl fmt::Debug for Trip {
     }
 }
 
-impl Trip {
-    pub fn add(id: u32, start: DateTime<Utc>) -> Trip {
-        // Add a Trip who is open
-        Trip {
-            id: id,
-            start: start,
-            end: None,
-            is_open: true,
+pub struct Trips {
+    // A Trips modelation
+    pub vec: Vec<Trip>,
+}
+
+impl fmt::Debug for Trips {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Trips").field("vec", &self.vec).finish()
+    }
+}
+
+impl Trips {
+    pub fn new() -> Trips {
+        // Create a new Trips
+        Trips { vec: Vec::new() }
+    }
+
+    pub fn post(&mut self, id: u32, start: DateTime<Utc>, end: Option<DateTime<Utc>>) {
+        // Add a Trip to the Trips
+        if end == None {
+            self.vec.push(Trip {
+                id: id,
+                start: start,
+                end: end,
+                is_open: true,
+            });
+        } else {
+            self.vec.push(Trip {
+                id: id,
+                start: start,
+                end: Some(end.unwrap()),
+                is_open: false,
+            });
         }
     }
 
-    pub fn end_trip(&mut self, end: DateTime<Utc>) {
-        // Add the final date and close a Trip
-        self.end = Some(end);
-        self.is_open = false
+    pub fn get(&self, id: u32) -> Option<&Trip> {
+        // Get a Trip from the Trips
+        self.vec.iter().find(|trip| trip.id == id)
+    }
+
+    pub fn put(&mut self, id: u32, start: Option<DateTime<Utc>>, end: Option<DateTime<Utc>>) {
+        // Update a Trip in the Trips
+        for trip in &mut self.vec {
+            if trip.id == id {
+                if start != None {
+                    trip.start = start.unwrap();
+                } else if end != None {
+                    trip.end = Some(end.unwrap());
+                    trip.is_open = false;
+                } else if end != None && start != None {
+                    trip.start = start.unwrap();
+                    trip.end = Some(end.unwrap());
+                    trip.is_open = false;
+                }
+            }
+        }
+    }
+
+    pub fn delete(&mut self, id: u32) {
+        // Delete a Trip from the Trips
+        self.vec.retain(|trip| trip.id != id);
     }
 }
