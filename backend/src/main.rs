c54d12ad36b335cd88c6d28104ae7f08abf70c00
Conrado Manclossi <conrado.macalossi@gmail.com>
2022-06-06 02:30:43 +0000

    api started

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,32 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Html, response::Json, routing::get, routing::post, Router, Server};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+
+#[derive(Deserialize, Serialize)]
+struct CreateUser {
+    name: String,
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<p>Hello, World!!!</p>")
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> Json<CreateUser> {
+    Json(CreateUser { name: payload.name })
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/user", post(create_user));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
-    axum::Server::bind(&addr)
+    Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-
-}
-    
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!!!</h1>")
 }
